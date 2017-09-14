c84dfe31392f3c5c1d1e5033776645cf6fc48a8f
Johan Tell <johan@mintcore.se>
2017-09-14 13:36:19 +0200

    Version bump to 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Metatags.Mixfile do
   def project do
     [
       app: :metatags,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
