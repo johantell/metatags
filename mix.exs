97d4dec6a5dfdda43fb315e97e96109d53aaa524
Johan Tell <johan@mintcore.se>
2018-06-06 20:30:00 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,50 +1,47 @@
 defmodule Metatags.Mixfile do
   use Mix.Project
 
+  @description """
+  Metatags provides an easy to work with API to set default and
+  page specific metatags on a page.
+  """
+
   def project do
     [
       app: :metatags,
       version: "0.1.2",
       elixir: "~> 1.4",
       deps: deps(),
-      description: description(),
+      description: @description,
       package: package(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
-        "coveralls": :test,
+        coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
         "coveralls.html": :test
-      ],
+      ]
     ]
   end
 
   defp deps do
     [
       {:phoenix_html, "~> 2.9"},
-
       {:ex_doc, "~> 0.16", only: :dev},
-
       {:credo, "~> 0.9.0", only: :dev},
       {:excoveralls, ">= 0.0.0", only: :test},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
     ]
-  end
-
-  defp description do
-    """
-    Metatags provides an easy to work with API to set default and
-    page specific metatags on a page.
-    """
   end
 
   defp package do
     [
       name: :metatags,
-      description: description(),
+      description: @description,
       files: ["lib", "mix.exs", "README*", "LICENSE*"],
       maintainers: ["johan Tell"],
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/mintcore/metatags"}
+      links: %{"GitHub" => "https://github.com/johantell/metatags"}
     ]
   end
 end
