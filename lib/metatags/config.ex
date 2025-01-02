f3e44e73864f17ed2e4856082b7f747963d97350
Johan Tell <1777129+johantell@users.noreply.github.com>
2025-01-02 09:50:41 +0100

    Move `Conn`-related functions behind the `Transport` protocol (#160)
    Background:
    In order to move forward to support phoenix live view, this change will
    create a `Transport` protocol which will act as an adapter and make it
    possible to make metatags more flexible by allowing different structs
    acting as a transport layer of metatags throughout a request life cycle.
    
    Part of #156

--- lib/metatags/config.ex
+++ lib/metatags/config.ex
@@ -8,4 +8,9 @@ defmodule Metatags.Config do
           title_separator: String.t(),
           metatags: %{}
         }
+
+  @spec put_meta(t(), String.t() | atom(), any()) :: t()
+  def put_meta(%__MODULE__{} = config, key, value) do
+    put_in(config, [Access.key!(:metatags), to_string(key)], value)
+  end
 end
---
+++ lib/metatags/transport.ex
@@ -0,0 +1,14 @@
+defprotocol Metatags.Transport do
+  @moduledoc """
+  Functions for transporting metatada along with the request lifecycle,
+  whether it be a `Plug.Conn`, a `Phoenix.LiveView.Socket` or something else.
+  """
+
+  @type t :: struct()
+
+  @spec put(t(), String.t() | atom(), any()) :: t()
+  def put(transport, key, value)
+
+  @spec get_metatags(t()) :: Metatags.Config.t()
+  def get_metatags(transport)
+end
