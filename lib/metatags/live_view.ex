4890913e0314333356a213edc6a8de3e541579ed
Johan Tell <johan@mintcore.se>
2025-09-03 13:29:56 +0200

    Update documentaion

--- lib/metatags/live_view.ex
+++ lib/metatags/live_view.ex
@@ -1,6 +1,24 @@
 defmodule Metatags.LiveView do
+  @moduledoc """
+  Live-view specific helpers meant to be used by the integrating application.
+
+  currently only contains the `on_mount/4` hook which will be called through the
+  routers `live_session` function:
+
+  ```elixir
+  @default_metatags Application.compile_env(:my_app, Metatags)
+  live_session :default, on_mount: [{Metatags.LiveView, {:init, @default_metatags}}] do
+    # ...
+  end
+  ```
+  """
   import Phoenix.LiveView, only: [attach_hook: 4]
 
+  @doc """
+  callback to be ran when mounting a live view. It initializes the live view
+  configuration onto the socket and attaches a hook on `handle_params` to automatically
+  put the canonical uri into the configuration.
+  """
   def on_mount({:init, default_metatags}, _params, _session, socket) do
     socket =
       socket
